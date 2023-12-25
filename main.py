d9f742b174409c438a274189b3e5b9f12c260bae
shakrocode <konradmisztal111@gmail.com>
2023-12-25 12:41:30 +0100

    enter int value

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 """main file for project """
 
+x = input('Wprowadż liczbę całkowitą...')
+if type(int(x)) is type(1):
+    x = int(x)
+    print(f'Wprowadzono liczbę {x}')
+else:
+    print('To nie jest liczba całkowita')
+input('Kliknij enter aby wyjść zprogramu')
+
